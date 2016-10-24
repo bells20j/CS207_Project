f6f507b176e6dedf92859591cc25863b04ef1a87
rileyherman <rileyherman0@gmail.com>
2016-10-24 15:06:49 -0600

    Add files via upload

--- rockband_analog_test.ino
+++ rockband_analog_test.ino
@@ -14,20 +14,19 @@ void setup() {
 }
 
 void loop() {
-  g_out = b_out = y_out = r_out = 0;
   g_out = analogRead(greenP);
   b_out = analogRead(blueP);
   y_out = analogRead(yellowP);
   r_out = analogRead(redP);
-  Serial.print("Green: ");
-  Serial.print(g_out);
-  Serial.print("\t");
-  Serial.print("Blue: ");
-  Serial.print(b_out);
-  Serial.print("\t");
-  Serial.print("Yellow: ");
-  Serial.print(y_out);
-  Serial.print("\t");
-  Serial.print("Red: ");
-  Serial.println(r_out);
+//  Serial.print("Green: ");
+  Serial.print(g_out, DEC);
+//  Serial.print("\t");
+//  Serial.print("Blue: ");
+  Serial.print(b_out, DEC);
+//  Serial.print("   \t");
+//  Serial.print("Yellow: ");
+  Serial.print(y_out, DEC);
+//  Serial.print("\t");
+//  Serial.print("Red: ");
+  Serial.println(r_out, DEC);
 }
